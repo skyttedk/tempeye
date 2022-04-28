2eec46aabbe329aa6128a494582f69af232a6a6d
Sigurd <kss@relateit.dk>
2022-04-28 10:45:30 +0200

    update

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//QR scanner
+
+//References
+/*
+    //QR
+    https://blog.minhazav.dev/research/html5-qrcode.html
+    https://github.com/mebjas/html5-qrcode
+
+*/
+Html5Qrcode.getCameras().then(devices => {
+
+
+
+
+  //Start scanning
+  const html5QrCode = new Html5Qrcode("reader");
+  html5QrCode.start({ facingMode: "environment" }, {
+        fps: 10, // Optional, frame per seconds for qr code scanning
+        qrbox: {
+          width: 250,
+          height: 250
+        } // Optional, if you want bounded box UI
+      },
+      (decodedText, decodedResult) => {
+        // do something when code is read
+        alert(decodedText)
+      },
+      (errorMessage) => {})
+    .catch((err) => {});
+}).catch(err => {
+  // handle err
+});
