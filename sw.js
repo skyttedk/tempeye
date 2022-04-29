46c17c335f9685a9b24515febced7522b93e2ed3
Sigurd <kss@relateit.dk>
2022-04-29 10:35:17 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 var cacheName = 'TempEye';
-var filesToCache = [];
+var filesToCache = [
+  '/'
+];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
